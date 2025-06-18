a7df32b36248fd64afc26c00f65c795f6c2c5024
SashexSRB <jovancicaleksandar@gmail.com>
2025-06-18 10:04:39 +0200

    Wipe all

--- bootloader/minimal.c
+++
@@ -1,9 +0,0 @@
-#include <efi.h>
-#include <efilib.h>
-
-EFI_STATUS EFIAPI efi_main(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable) {
-    InitializeLib(ImageHandle, SystemTable);
-    Print(L"Minimal UEFI App Started!\n");
-    SystemTable->BootServices->Stall(5 * 1000 * 1000); // 5 seconds
-    return EFI_SUCCESS;
-}
